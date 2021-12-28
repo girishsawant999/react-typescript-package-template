06e8402ddcf48e5a790c01a8dea5c198f205d8b9
Girish Sawant <girishsawant999.gs@gmail.com>
2021-12-28 15:23:23 +0530

    test: :white_check_mark: Updated test cases for the base component

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -1,3 +1,15 @@
-test('adds 1 + 2 to equal 3', () => {
-  expect(1 + 2).toBe(3);
+import BaseComponent from '../';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+describe('Test for base component', () => {
+  it('should render the base component without crash', () => {
+    const div = document.createElement('div');
+
+    ReactDOM.render(<BaseComponent />, div);
+    expect(div.querySelector('h2').textContent).toBe(
+      'Welcome To react loading button',
+    );
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
